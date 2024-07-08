14f260785f3c2bf4e3b304b6e1a6b1260d96b0c6
unknown <mirfanpebriadi@student.uir.ac.id>
2024-07-08 17:01:28 +0700

    commit baru

--- src/App.vue
+++ src/App.vue
@@ -1,170 +1,180 @@
 <template>
-  <q-layout view="hHh lpR fFf">
-    <q-header reveal elevated class="bg-primary text-white" height-hint="98">
-      <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+  <q-layout>
+  <div class="q-pa-md">
 
-        <q-toolbar-title> pathanShop </q-toolbar-title>
+      <q-header elevated style="position: fixed; width: 100%; z-index: 1000;" class="header">
+        <q-toolbar>
+          <q-btn flat round dense icon="menu" class="q-mr-sm" />
+          <q-toolbar-title> ANIME FIGURES</q-toolbar-title>
+          <q-btn flat round dense icon="whatshot" />
+        </q-toolbar>
 
-        <q-space />
+      </q-header>
+      
+      <q-page-container>
+        <q-page class="q-pa-md">
+          <div class="q-pa-md">
+            <div class="q-gutter-md">
+            <q-carousel class="banner"
+              animated
+              v-model="slide"
+              arrows
+              navigation
+              infinite
+              >
+              <q-carousel-slide name="style" class="column no-wrap flex-center">
+                <q-img 
+                  src="https://nerdarena.in/cdn/shop/files/Website-banner_anime.jpg?v=1657633543&width=1920" 
+                  class="banner"
+                />
+                <div class="q-mt-md text-center">
+                  {{ lorem }}
+                </div>
+              </q-carousel-slide>
+              <q-carousel-slide name="tv" class="column no-wrap flex-center">
+                <q-img class="banner"
+                  src="https://animefigure.co.uk/wp-content/uploads/2023/07/bANNER-1.jpg" 
+                />
+                <div class="q-mt-md text-center">
+                  {{ lorem }}
+                </div>
+              </q-carousel-slide>
+            </q-carousel>
 
-        <q-btn flat label="Daftar" />
-        <q-btn flat label="Log In" />
+          </div>
 
-        <q-input round filled dense placeholder="Search..." />
-        <q-btn flat round icon="search" />
-      </q-toolbar>
+          <div class="q-pa-md row items-start q-gutter-md">
 
-      <q-tabs align="left">
-        <q-route-tab to="/page1" label="Notifikasi" />
-        <q-route-tab to="/page2" label="Bantuan" />
-        <q-route-tab to="/page3" label="Kontak" />
-      </q-tabs>
-    </q-header>
+            <q-card class="my-card">
+              <q-img src="https://animefigure.co.uk/wp-content/uploads/2023/06/Naruto-Itachi-Anime-Figur-30CM-Uchiha-Sasuke-Uzumaki-Naruto-PVC-Action-Shippuden-Vibration-Sterne-Figurine-Modell-1-441x441.jpg">
+                <div class="absolute-bottom text-subtitle2 text-center">
+                  Uzumaki Naruto
+                </div>
+              </q-img>
+            </q-card>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
-    </q-drawer>
+            <q-card class="my-card">
+              <q-img src="https://animefigure.co.uk/wp-content/uploads/2023/05/anime-figure-demon-slayer-figures-battle-nezuko-kamado-blood-demon-441x441.jpg">
+                <div class="absolute-bottom text-subtitle2 text-center">
+                  Nezuko Kamado
+                </div>
+              </q-img>
+            </q-card>
 
-    <q-page-container>
-      <div class="q-pa-md" style="margin-bottom: 1rem">
-        <div class="q-gutter-md">
-          <q-carousel
-            v-model="slide"
-            transition-prev="jump-right"
-            transition-next="jump-left"
-            swipeable
-            animated
-            control-color="white"
-            prev-icon="arrow_left"
-            next-icon="arrow_right"
-            navigation-icon="radio_button_unchecked"
-            navigation
-            padding
-            arrows
-            height="300px"
-            class="bg-purple text-white shadow-1 rounded-borders"
-          >
-            <q-carousel-slide name="style" class="column no-wrap flex-center">
-              <q-icon name="style" size="56px" />
-              <div class="q-mt-md text-center">
-                {{ lorem }}
-              </div>
-            </q-carousel-slide>
-            <q-carousel-slide name="tv" class="column no-wrap flex-center">
-              <q-icon name="live_tv" size="56px" />
-              <div class="q-mt-md text-center">
-                {{ lorem }}
-              </div>
-            </q-carousel-slide>
-            <q-carousel-slide name="layers" class="column no-wrap flex-center">
-              <q-icon name="layers" size="56px" />
-              <div class="q-mt-md text-center">
-                {{ lorem }}
-              </div>
-            </q-carousel-slide>
-            <q-carousel-slide name="map" class="column no-wrap flex-center">
-              <q-icon name="terrain" size="56px" />
-              <div class="q-mt-md text-center">
-                {{ lorem }}
-              </div>
-            </q-carousel-slide>
-            <q-carousel-slide name="clock" class="column no-wrap flex-center">
-              <q-icon name="schedule" size="56px" />
-              <div class="q-mt-md text-center">
-                {{ lorem }}
-              </div>
-            </q-carousel-slide>
-          </q-carousel>
-        </div>
-      </div>
-    </q-page-container>
+            <q-card class="my-card">
+              <q-img src="https://animefigure.co.uk/wp-content/uploads/2023/07/Anime-Nika-Ruffy-Gear-5-Action-figur-Spielzeug-ein-St-ck-Figuren-20cm-gk-Statue-Manga-1-441x441.jpg">
+                <div class="absolute-bottom text-subtitle2 text-center">
+                  Nika Luffy Gear 5
+                </div>
+              </q-img>
+            </q-card>
 
-    <q-footer elevated class="bg-grey-8 text-white text-center q-pa-md">
-      <em> @copyright </em> M Irfan Pebriadi
-    </q-footer>
-  </q-layout>
+                        <q-card class="my-card">
+              <q-img src="https://animefigure.co.uk/wp-content/uploads/2023/06/48cm-One-Piece-Abbildung-Lorenor-Zorro-Neun-Messer-Fluss-Pop-Max-Asura-Zoro-Action-figuren-PVC-1-441x441.jpg">
+                <div class="absolute-bottom text-subtitle2 text-center">
+                  Rononoa Zoro
+                </div>
+              </q-img>
+            </q-card>
+            <q-card class="my-card">
+              <q-img src="https://animefigure.co.uk/wp-content/uploads/2023/06/32cm-Anime-Einem-St-ck-Tiere-Piraten-K-nig-Vier-Kaiser-Kaido-King-Figur-YAN-ZAI-1-441x441.jpg">
+                <div class="absolute-bottom text-subtitle2 text-center">
+                  King Four Emperor Kaido 
+                </div>
+              </q-img>
+            </q-card>
+            <q-card class="my-card">
+              <q-img src="https://cdn.kyou.id/thumbnail/items/43225-pvc-figure-17-megumi-kato-first-meeting-outfit-ver.jpg.webp">
+                <div class="absolute-bottom text-subtitle2 text-center">
+                  Megumi Kato - First Meeting Outfit
+                </div>
+              </q-img>
+            </q-card>
+            <q-card class="my-card">
+              <q-img src="https://cdn.kyou.id/items/147061-pvc-figure-14-suguru-geto-jujutsu-kaisen-0.jpg.webp">
+                <div class="absolute-bottom text-subtitle2 text-center">
+                  Suguru Geto
+                </div>
+              </q-img>
+            </q-card>
+            <q-card class="my-card">
+              <q-img src="https://cdn.kyou.id/items/195328-pvc-figure-17-makima-chainsaw-man.jpg.webp">
+                <div class="absolute-bottom text-subtitle2 text-center">
+                  Makima
+                </div>
+              </q-img>
+            </q-card>
+            <q-card class="my-card">
+              <q-img src="https://cdn.kyou.id/items/136224-s-fire-figure-18-bakugo-katsuki-boku-no-hero-academia.jpg.webp">
+                <div class="absolute-bottom text-subtitle2 text-center">
+                  Bakugo Katsuki
+                </div>
+              </q-img>
+            </q-card>
 
-  <!-- New Card Section -->
-  <div class="q-pa-md row items-start q-gutter-md" style="margin-top: 1rem">
-    <q-card class="my-card" flat bordered v-for="i in 5" :key="i">
-      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" />
+            <q-card class="my-card">
+              <q-img src="https://cdn.kyou.id/items/114793-exclusive-sale-pvc-figure-the-ghost-bride-illustration-by-fuzichoco.jpg.webp">
+                <div class="absolute-bottom text-subtitle2 text-center">
+                  Figure The Ghost Bride Illustration by fuzichoco
+                </div>
+              </q-img>
+            </q-card>
 
-      <q-card-section>
-        <div class="text-overline text-orange-9">Overline</div>
-        <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
-        <div class="text-caption text-grey">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </div>
-      </q-card-section>
-
-      <q-card-actions>
-        <q-btn flat color="primary" label="Share" />
-        <q-btn flat color="secondary" label="Book" />
-
-        <q-space />
-
-        <q-btn
-          color="grey"
-          round
-          flat
-          dense
-          :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-          @click="expanded = !expanded"
-        />
-      </q-card-actions>
-
-      <q-slide-transition>
-        <div v-show="expanded">
-          <q-separator />
-          <q-card-section class="text-subtitle2">
-            {{ lorem }}
-          </q-card-section>
-        </div>
-      </q-slide-transition>
-    </q-card>
+          </div>
+          </div>
+        </q-page>
+      </q-page-container>
+ 
   </div>
-  <!-- End of New Card Section -->
+  <q-footer reveal elevated>
+        <q-toolbar class="footer">
+          <q-toolbar-title>Thyo aji Dinata -- 223510553</q-toolbar-title>
+        </q-toolbar>
+      </q-footer>
+
+</q-layout>
+  <q-footer elevated>
+    <q-toolbar class="glossy">
+      <q-toolbar-title>Footer</q-toolbar-title>
+    </q-toolbar>
+  </q-footer>
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 
 export default {
   setup() {
-    const leftDrawerOpen = ref(false);
-    const slide = ref(0); // Add this line for carousel navigation
-    const expanded = ref(false); // Add this line for expanded section
-    const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."; // Add this line for lorem text
-
-    onMounted(() => {
-      // Tutup sidebar saat pertama kali halaman dimuat
-      leftDrawerOpen.value = false;
-    });
-
-    const toggleLeftDrawer = () => {
-      leftDrawerOpen.value = !leftDrawerOpen.value;
-    };
-
     return {
-      leftDrawerOpen,
-      slide,
-      expanded,
-      lorem,
-      toggleLeftDrawer,
+      tab: ref("images"),
+      time: ref("10:56"),
+      timeWithSeconds: ref("09:24:10"),
+      slide: ref("style"),
+      lorem: "",
     };
   },
 };
 </script>
 
 <style>
-/* Add your custom styles here */
-.my-card {
-  flex: 1;
-  max-width: 300px;
+.header {
+  background-image: linear-gradient(120deg, #3b3131 0%, #db1d17 100%);
 }
-.q-gutter-md {
-  margin: 0 -1rem; /* Set margin between carousel and cards to 1rem */
+
+.my-card {
+  width: 100%;
+  max-width: 250px;
+}
+
+
+.banner {
+  border-radius: 16px;
+  content: center;
+  align-self: center;
+}
+
+.footer {
+  background-image: linear-gradient(120deg, #3b3131 0%, #db1d17 100%);
+  text-align: center;
 }
 </style>
